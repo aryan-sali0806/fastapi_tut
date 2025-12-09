559c4256fe9bb063b3e18bcb29e2d61e6ab51e4f
aryan-sali0806 <aryansali0808@gmail.com>
2025-12-10 00:47:28 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI,Path,HTTPException,Query
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data=json.load(f)
+    
+    return data
+
+@app.get("/")
+def hello():
+    return {'message':"Patient Management System API"}
+
+@app.get("/about")
+def about():
+    return {'massage':'A fully functional API to manage patient records'}
+
+@app.get("/view")
+def view():
+    data=load_data()
+    return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str=Path(...,description='ID of patient in DB',example='P001')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail='patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by: str=Query(...,description='sort on basis of height,weight or bmi'),order:str=Query('asc',description='sort in ascending or descending order')):
+    valid_fields=['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f"invalid field select from {valid_fields}")
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail=f"invalid field select")
+    sort_order=True if order=='desc' else False
+    data=load_data()
+    sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
+
